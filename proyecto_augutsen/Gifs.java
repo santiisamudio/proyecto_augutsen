67063896550ba49c51a85d6bb5cc27449e822465
mikitalinda <138534373+mikitalinda@users.noreply.github.com>
2025-04-06 22:48:55 -0300

    Add files via upload

---
+++ proyecto_augutsen/Gifs.java
@@ -0,0 +1,116 @@
+package proyecto_augutsen;
+
+import java.util.HashMap;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class Gifs {
+    private HashMap<Long, ImageView> _imagenes = new HashMap<>();
+    private Pane _contenedor;
+    private ImageView _gifPrincipal;
+    private Gifs gifView;
+    public Gifs(Pane contenedor) {
+        this._contenedor = contenedor;
+        this._gifPrincipal = new ImageView();
+    }
+
+    public void CrearGif(int id, double x, double y, long idSesion, String rutaGif) {
+    	//chequear que onda este if
+    	 if (_imagenes.containsKey(idSesion)) return;
+    	 Image gifImage = new Image(getClass().getResource(rutaGif).toExternalForm());  
+         ImageView gifView = new ImageView(gifImage); 
+
+        gifView.setId("pieza");
+        gifView.setLayoutX(x);  // Posición X correcta para un Pane
+        gifView.setLayoutY(y);  // Posición Y correcta para un Pane
+        gifView.setVisible(false);
+        gifView.setFitWidth(250);  // Ajustar tamaño
+        gifView.setFitHeight(250);
+        gifView.setPreserveRatio(true);
+
+        this._imagenes.put(idSesion, gifView);  // Guardamos el GIF en el HashMap
+        this._contenedor.getChildren().add(gifView);  // Agregamos el GIF al contenedor
+    }
+
+    public ImageView getGif(long sesionId) {
+        return this._imagenes.get(sesionId);
+    }
+
+    public void LimpiarContenedor() {
+        this._imagenes.clear();
+        this._contenedor.getChildren().clear(); // Limpiar visualmente también
+    }
+
+    public ImageView EliminarGif(long sesionId) {
+        ImageView gif = this._imagenes.remove(sesionId);
+        if (gif != null) {
+            this._contenedor.getChildren().remove(gif);
+        }
+        return gif;
+    }
+
+    public void ConfigurarGifPrincipal(double ancho, double alto) {
+        this._gifPrincipal.setPreserveRatio(false);
+        this._gifPrincipal.setFitWidth(ancho);
+        this._gifPrincipal.setFitHeight(alto);
+        this._gifPrincipal.setId("background");
+        this._contenedor.getChildren().add(this._gifPrincipal);
+    }
+    public void AsignarGif_nivel0() {
+    	 this.gifView = new Gifs(this._contenedor); 
+         double gifX = 0.097 * this._contenedor.getWidth();  // centro X del cuadrante sup izq
+         double gifY = 0.11 * this._contenedor.getHeight();   // centro Y del cuadrante sup izq
+         this.gifView.CrearGif(999, gifX, gifY, 999, "/gifs/4RNk.gif");
+         ImageView gif = this.gifView.getGif(999);
+         if (gif != null) {
+             gif.setVisible(true);
+             gif.setLayoutX(30);  // CORREGIDO
+             gif.setLayoutY(270);  // CORREGIDO
+         }
+
+         double gifX2 = 0.5 * this._contenedor.getWidth();
+         double gifY2 = 0.5 * this._contenedor.getHeight();
+         this.gifView.CrearGif(888, gifX2, gifY2, 888, "/gifs/Z23b.gif");
+         ImageView gif2 = this.gifView.getGif(888);
+         if (gif2 != null) {
+             gif2.setVisible(true);
+             gif2.setLayoutX(30);
+             gif2.setLayoutY(650); }
+         //gif3
+         double gifX3 = 0.5 * this._contenedor.getWidth();
+         double gifY3 = 0.5 * this._contenedor.getHeight();
+         this.gifView.CrearGif(777, gifX3, gifY3,777, "/gifs/VJl.gif");
+         ImageView gif3 = this.gifView.getGif(777);
+         if (gif3 != null) {
+             gif3.setVisible(true);
+             gif3.setLayoutX(1640);
+             gif3.setLayoutY(650); }
+    }
+    public void AsignarGif_nivel1() {
+    	double gifX_4 = 0.25 * this._contenedor.getWidth();
+		  double gifY_4 = 0.75 * this._contenedor.getHeight();
+		  this.gifView.CrearGif(1111, gifX_4, gifY_4, 1111, "/gifs/VJE4.gif");
+		  ImageView gif_4 = this.gifView.getGif(1111);
+		  gif_4.setScaleX(1.3);
+		    if (gif_4 != null) {
+		        gif_4.setVisible(true);
+		        gif_4.setLayoutX(100);
+		        gif_4.setLayoutY(700);
+		    }
+
+		    double gifX_5 = 0.55 * this._contenedor.getWidth();
+		    double gifY_5 = 0.78 * this._contenedor.getHeight();
+		    this.gifView.CrearGif(1112, gifX_5, gifY_5, 1112, "/gifs/7SUp.gif");
+		    ImageView gif_5 = this.gifView.getGif(1112);
+		    gif_5.setScaleX(1.2); // Aumenta el tamaño al 150% horizontalmente
+		    gif_5.setScaleY(1.2); // Aumenta el tamaño al 150% verticalmente
+
+		    if (gif_5 != null) {
+		        gif_5.setVisible(true);
+		        gif_5.setLayoutX(1590);
+		        gif_5.setLayoutY(730);
+		    }
+    }
+    
+}
---
+++ proyecto_augutsen/ListenerTuio.java
@@ -0,0 +1,221 @@
+package proyecto_augutsen;
+
+import TUIO.TuioBlob;
+import TUIO.TuioCursor;
+import TUIO.TuioListener;
+import TUIO.TuioObject;
+import TUIO.TuioTime;
+import javafx.application.Platform;
+import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
+import javafx.stage.Screen;
+
+public class ListenerTuio implements TuioListener{
+    private Pane _contenedor;//contenedor
+    private int _nivel;
+    private AplicacionMain _main;
+    private ImagenesV imagenV;
+    private PeriodicoInicial periodico;
+    private Emociones _emociones;
+    //mica
+    private Gifs gifView;
+    
+    
+    public ListenerTuio(Pane contenedor,AplicacionMain main) {    	
+    	this.periodico = new PeriodicoInicial();
+    	this._nivel= 0;
+    	this._main= main;
+        this._contenedor = contenedor;
+        this._emociones = new Emociones();
+        double ancho = Screen.getPrimary().getVisualBounds().getWidth();
+        double alto = Screen.getPrimary().getVisualBounds().getHeight();    
+        
+        this.imagenV = new ImagenesV(this._contenedor);       
+        this.imagenV.ConfigurarImagenPrincipal(ancho,alto);
+        this.imagenV.AsignarImagenPrincipal();
+        //mica
+        this.gifView = new Gifs(this._contenedor); 
+        this.gifView.AsignarGif_nivel0(); 
+       
+    }
+    
+    @Override
+    public void addTuioObject(TuioObject to) {
+        System.out.println("Object added: " + to.getSymbolID());
+        Platform.runLater(() -> {/*javaFX solo permite actualizar la UI desde si propio thread para evitar errores, esta linea hace que el codigo se ejecute en el thread principal,
+                                    TUIO ejecuta las funciones desde un thread secundario*/
+            int id_simbolo= to.getSymbolID();
+            double x = to.getX() * this._contenedor.getWidth();/*TUIO usa coordenadas de 0.0(arriba izquierda) a 1.1(abajo derecha) el contenedor de javafx tiene como coordenadas
+                                                                    pixeles, para convertirlo se multiplican, si x=0.5 , 0.5 * 800 pixeles = 400*/
+            double y = to.getY() * this._contenedor.getHeight();
+            this.imagenV.CrearImagen(id_simbolo, x, y, to.getSessionID());
+        	ImageView imageV = this.imagenV.getImagen(to.getSessionID());   
+        
+        	//hasta aca mica
+                if((to.getSymbolID()!=3)&&(this._nivel==0)&&(esCuadranteCentroMapa(to.getX(),to.getY()))){//se pasa la ubicacion del objeto TUIO para ver si se encuentra abajo a la derecha                    
+                	if (imageV != null) {//verifica que el objeto exista 
+                
+                		imageV.setVisible(true);
+                        imageV.setX(to.getX() * this._contenedor.getWidth());  // Posición X de la imagen
+                        imageV.setY(to.getY() * this._contenedor.getHeight());  // Posición Y de la imagen
+                        this.periodico.agregarMapa(to.getSymbolID(),to.getX(),to.getY(),to.getAngleDegrees());                        
+                        if (this.periodico.estaCompleto() ) 
+                        	this.subirNivel();                        
+                	}
+                   
+                }else if((to.getSymbolID()==3)&&(this._nivel==1)&&(esCuadranteInferiorIzquierdo(to.getX(),to.getY()))){
+                	
+                	
+                	if(imageV != null){//verifica que el objeto exista 
+                		imageV.setVisible(true);
+                		imageV.setX(to.getX() * this._contenedor.getWidth());//se actualiza x
+                		imageV.setY(to.getY() * this._contenedor.getHeight());//se actualiza y
+                		
+                		this.imagenV.AsignarImagenRotacion(to.getAngleDegrees());
+                	}
+                
+                }      
+        });
+    }
+    
+    
+    private void subirNivel() {
+    	this._nivel++;
+		this.imagenV.AsignarImagenAugutsen();
+    	Platform.runLater(() ->this._main.iniciarVideo());
+		this._contenedor.getChildren().removeIf(node -> node instanceof ImageView && !node.getId().equals("background"));//elimina las partes del rompecabezas del contenedor
+		this.imagenV.LimpiarContenedor();//limpia la imagen
+		//AGREGO MAS GIFS
+		this.gifView.AsignarGif_nivel1();
+    }
+    
+    private boolean esCuadranteCentroMapa(double x, double y) {
+    	return ((x>0.1674)&&(x<0.8330)&&(y>0.1241)&&(y<0.8878));
+    }
+    
+    private boolean esCuadranteInferiorIzquierdo(double x, double y){
+        return ((x>0.0195)&&(x<0.210)&&(y>0.6570)&&(y<0.930));
+    }
+  
+
+    @Override   
+   
+    public void updateTuioObject(TuioObject obj) { 
+        double x = obj.getX();
+        double y = obj.getY();
+        double angle = obj.getAngleDegrees();  // Guardamos el ángulo para comparar cambios
+
+        Platform.runLater(() -> {  
+            
+            ImageView imageV = this.imagenV.getImagen(obj.getSessionID());
+            if (imageV != null) {
+                if (obj.getSymbolID() != 3) {
+                    if (this._nivel == 0) {	
+                        boolean dentroDelCuadrante = esCuadranteCentroMapa(x, y);
+
+                        // Solo actualizar si hay cambios para evitar repaints innecesarios
+                        if (dentroDelCuadrante) {
+                            
+                            imageV.setVisible(true);
+                            
+                            imageV.setX(x * this._contenedor.getWidth());
+                            imageV.setY(y * this._contenedor.getHeight());
+
+                            this.periodico.agregarMapa(obj.getSymbolID(), x, y, obj.getAngleDegrees());
+                            if (this.periodico.estaCompleto() ) 
+                            	this.subirNivel();
+                            
+                        } else {
+                            imageV.setVisible(false);
+                            this.periodico.eliminarMapa(obj.getSymbolID());
+                            }
+                    		}else{
+                    
+                    			 if(this.esCuadranteCentroMapa(x, y)) {
+                    				System.out.print("entra al if");
+                    				this._emociones.detectarObjetoEmocion(obj.getSymbolID(), this.imagenV);
+                    			 }
+                    }
+                } else { // Si el símbolo es 3
+                    if (this._nivel == 1) {
+                        boolean dentroDelCuadrante = esCuadranteInferiorIzquierdo(x, y);
+
+                        if (dentroDelCuadrante) {
+                            imageV.setVisible(true);
+                            imageV.setX(x * this._contenedor.getWidth());
+                            imageV.setY(y * this._contenedor.getHeight());
+
+                            // Evitar actualizaciones constantes de rotación si el cambio es mínimo
+                            double anguloActual = imageV.getRotate();
+                            if (Math.abs(anguloActual - angle) > 1) { 
+                               // imageV.setRotate(angle);
+                                
+                                this.imagenV.AsignarImagenRotacion(obj.getAngleDegrees());
+                                
+                                
+                            }
+                            
+                        }
+                        else {
+                        	imageV.setVisible(false);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    
+    
+
+
+    @Override
+    public void removeTuioObject(TuioObject obj) {// un objeto se elimina automaticamente cuando desaparece del campo visual
+       
+        long idSesion= obj.getSessionID();
+         Platform.runLater(() -> {
+            if(obj.getSymbolID()!=3){
+            	
+            	ImageView imageV = this.imagenV.EliminarImagen(idSesion);
+                if (imageV != null) {               	
+                    this._contenedor.getChildren().remove(imageV);//elimina al circulo del contenedor pane                    
+                    this.periodico.eliminarMapa(obj.getSymbolID());               
+                }
+            }else{
+            	ImageView imageV = this.imagenV.EliminarImagen(idSesion);
+                if (imageV != null) {
+                    this._contenedor.getChildren().remove(imageV);//elimina al circulo del contenedor pane
+                }
+            }   
+        }
+         )
+;
+    
+}
+
+    @Override
+    public void addTuioCursor(TuioCursor tc) {
+    System.out.println("Cursor added: " + tc.getCursorID());
+            }
+
+    @Override
+    public void updateTuioCursor(TuioCursor tc) {
+    System.out.println("Cursor updated: " + tc.getCursorID() + " at " + tc.getX() + ", " + tc.getY());
+    }
+
+    @Override
+    public void removeTuioCursor(TuioCursor tc) {}
+
+    @Override
+    public void addTuioBlob(TuioBlob tb) {}
+
+    @Override
+    public void updateTuioBlob(TuioBlob tb) {}
+
+    @Override
+    public void removeTuioBlob(TuioBlob tb) {}
+
+    @Override
+    public void refresh(TuioTime tt) {}
+    
+}
